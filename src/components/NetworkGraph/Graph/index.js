f8b7344128629a5d3b3a204b771ef542fb189fda
Mahdi-Ghanbarzadeh <mahdi.ghanbarzadeh.a@gmail.com>
2023-07-27 12:01:46 +0330

    node selection feature added

--- src/components/NetworkGraph/Graph/index.js
+++ src/components/NetworkGraph/Graph/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import * as d3 from "d3";
 import styles from "./Graph.module.css";
 
@@ -11,12 +11,15 @@ const GraphVisualization = ({ data }) => {
     width = 1100 - margin.left - margin.right,
     height = 700 - margin.top - margin.bottom;
 
+  // variable to keep track of clicked nodes in order to reset styles
+  let setClickedNode = null;
+
   // const width = 1100;
   // const height = 700; // Adjusted height to fit the graph inside the SVG element
   const edgeWidth = 2;
   const nodeRadius = 25;
-  const nodeLabelVisibility = false;
-  const edgeLabelVisibility = false;
+  const nodeLabelVisibility = true;
+  const edgeLabelVisibility = true;
 
   const getIcon = (device_type) => {
     switch (device_type) {
@@ -136,7 +139,110 @@ const GraphVisualization = ({ data }) => {
             d.fx = null;
             d.fy = null;
           })
-      );
+      )
+      .on("click", handleClick); // Add click event handler
+
+    // Click event handler
+    function handleClick(event, d) {
+      if (setClickedNode === d) {
+        // If the clicked node is already clicked, reset the styles
+        resetStyles();
+      } else {
+        if (setClickedNode !== null) {
+          // Reset the styles of the previously clicked node
+          resetStyles();
+        }
+
+        // Get the clicked node and its connected links
+        const clickedNode = d;
+        const connectedLinks = data.edges.filter(
+          (link) =>
+            link.source.id === clickedNode.id ||
+            link.target.id === clickedNode.id
+        );
+
+        // Extract the siblings from connected links
+        const siblingNodes = [clickedNode];
+        connectedLinks.forEach((link) => {
+          if (link.source.id === clickedNode.id) {
+            siblingNodes.push(link.target);
+          } else {
+            siblingNodes.push(link.source);
+          }
+        });
+
+        // Update visualization to highlight the clicked node and its siblings
+        node
+          .filter((node) => siblingNodes.includes(node))
+          .style("opacity", 1)
+          .style("filter", "none")
+          .select("circle") // Select the circle element within the node to apply styles
+          .style("fill", (node) =>
+            node === clickedNode ? "steelblue" : "steelblue"
+          ) // Use gray color for the clicked node
+          .style("stroke", (node) => (node === clickedNode ? "#ddd" : "#bbb"))
+          .style("stroke-width", (node) => (node === clickedNode ? 3.5 : 1.5));
+
+        node
+          .filter((node) => !siblingNodes.includes(node))
+          .style("opacity", 0.7)
+          .style("filter", "grayscale(70%)");
+
+        link
+          .filter(
+            (link) =>
+              siblingNodes.includes(link.source) &&
+              siblingNodes.includes(link.target)
+          )
+          .style("opacity", 1)
+          .style("filter", "none");
+
+        link
+          .filter(
+            (link) =>
+              !siblingNodes.includes(link.source) ||
+              !siblingNodes.includes(link.target)
+          )
+          .style("opacity", 0.05)
+          .style("filter", "grayscale(70%)");
+
+        linkLabels
+          .filter(
+            (link) =>
+              siblingNodes.includes(link.source) &&
+              siblingNodes.includes(link.target)
+          )
+          .style("opacity", 1)
+          .style("filter", "none");
+
+        linkLabels
+          .filter(
+            (link) =>
+              !siblingNodes.includes(link.source) ||
+              !siblingNodes.includes(link.target)
+          )
+          .style("opacity", 0.4)
+          .style("filter", "grayscale(70%)");
+
+        // Set the clicked node in order to reset
+        setClickedNode = clickedNode;
+      }
+    }
+
+    // Function to reset the styles
+    function resetStyles() {
+      node
+        .style("opacity", 1)
+        .style("filter", "none")
+        .select("circle")
+        .style("fill", "steelblue")
+        .style("stroke", "#bbb")
+        .style("stroke-width", 1);
+      // node.style("opacity", 1).style("filter", "none");
+      link.style("opacity", 0.6).style("filter", "none");
+      linkLabels.style("opacity", 1).style("filter", "none");
+      setClickedNode = null;
+    }
 
     // node.append("circle").attr("r", nodeRadius).style("fill", "steelblue");
 
@@ -192,6 +298,27 @@ const GraphVisualization = ({ data }) => {
         // Hide the tooltip when mouse leaves the node
         tooltip.transition().duration(500).style("opacity", 0);
       });
+
+    simulation.on("tick", () => {
+      // Update the position of the nodes during each tick
+      node.attr("transform", (d) => {
+        // Ensure nodes stay within SVG bounds
+        d.x = Math.max(20, Math.min(width - (nodeRadius + 10), d.x));
+        d.y = Math.max(20, Math.min(height - (nodeRadius + 10), d.y));
+        return `translate(${d.x},${d.y})`;
+      });
+
+      link
+        .attr("x1", (d) => d.source.x)
+        .attr("y1", (d) => d.source.y)
+        .attr("x2", (d) => d.target.x)
+        .attr("y2", (d) => d.target.y);
+
+      // Position the link labels at the midpoint between the source and target nodes
+      linkLabels
+        .attr("x", (d) => (d.source.x + d.target.x) / 2)
+        .attr("y", (d) => (d.source.y + d.target.y) / 2);
+    });
 
     simulation.on("tick", () => {
       link
